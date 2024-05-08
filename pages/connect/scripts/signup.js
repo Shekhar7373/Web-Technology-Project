106a52b88ce0f07a45caff6590dcd83d808d8747
Shekhar7373 <shekharmd7373@gmail.com>
2024-05-08 22:45:28 +0530

    Combined both sign-in and sign-up pages

--- pages/connect/scripts/signup.js
+++ pages/connect/scripts/signup.js
@@ -1,3 +1,17 @@
+const container = document.getElementById("container");
+const registerBtn = document.getElementById("register");
+const loginBtn = document.getElementById("login");
+
+registerBtn.addEventListener("click", () => {
+  container.classList.add("active");
+});
+
+loginBtn.addEventListener("click", () => {
+  container.classList.remove("active");
+});
+
+
+
 function validateAndStore() {
     const name = document.getElementById('name').value;
     const usn = document.getElementById('usn').value;
