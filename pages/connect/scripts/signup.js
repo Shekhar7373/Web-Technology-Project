7c801332cdad454e54261cf2857f4747d2fccf04
Shekhar7373 <shekharmd7373@gmail.com>
2024-05-10 12:27:07 +0530

    JS Validation is Added for Sign-in pages

--- pages/connect/scripts/signup.js
+++ pages/connect/scripts/signup.js
@@ -10,7 +10,20 @@ loginBtn.addEventListener("click", () => {
   container.classList.remove("active");
 });
 
+// For Demo purpose one student details is added to validate & verify
+var name = "a"
+var usn = "a"
+var mobile = "1234"
+var email = "sdm@gmail.com"
+var password = "a"
 
+function checkpassword(){
+  var passwd = document.getElementById("password").value;
+  var confirmPassword = document.getElementById("confirmPassword").value;
+  if(passwd != confirmPassword){
+    alert("Enter both password is not matching! correct it");
+  }
+}
 
 function validateAndStore() {
     const name = document.getElementById('name').value;
@@ -18,19 +31,35 @@ function validateAndStore() {
     const mobile = document.getElementById('mobile').value;
     const password = document.getElementById('password').value;
     const confirmPassword = document.getElementById('confirmPassword').value;
-
+    var option1 = document.getElementById('deptdropdown').value;
+    console.log(name, usn , mobile, password, confirmPassword, option1);
     if (password !== confirmPassword) {
         alert("Passwords do not match!");
         return false;
     }
-
-    const userData = {
-        name: name,
-        mobile: mobile,
-        password: password
-    };
-
-    localStorage.setItem(usn, JSON.stringify(userData));
     alert("Registration successful!");
     return false; // Prevent form submission for demonstration
+}
+
+function verify(){
+  var enteredusn = document.getElementById("chkusn").value;
+  var enteredpasswd = document.getElementById("chkpasswd").value;
+  if(enteredusn != usn || enteredpasswd != password){
+    alert("Invalid login details! try again");
+  }
+  else{
+    alert("Logged in successfully!Redirected to next page");
+    document.write("Successfull Login Test!!");
+  }
+}
+function checkallfields(){
+  var chkusn = document.getElementById("chkusn").value;
+  var chkpasswd = document.getElementsByTagName("chkpasswd").value;
+  var btn = document.getElementById("loginbtn");
+  if(chkpasswd == "" || chkusn == ""){
+    btn.disabled = true;
+  }
+  else{
+    btn.disabled = false;
+  }
 }
