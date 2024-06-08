23bf70c399acafd053b651a432d105f77b921091
Suraj Gunagi <surajgunagi7025@gmail.com>
2024-06-08 21:35:07 +0530

    ddd

--- src/connect/src/loginpage.php
+++ src/connect/src/loginpage.php
@@ -8,10 +8,10 @@ $database = "nexus student data";
 
 $conn = mysqli_connect($servername, $username, $password, $database);
 if (!$conn) {
-    die("Sorry unable to connect!!! ". mysqli_connect_error());
+    die("Sorry unable to connect!!! " . mysqli_connect_error());
 }
 echo "Connection succefull";
-echo"<br/>";
+echo "<br/>";
 // Connection built successfully..............
 
 //Verify the user that entered user is in Database using USN & Passwd
@@ -20,15 +20,15 @@ $usn = $_POST["usn"];
 $sql = "SELECT * FROM `signup data` WHERE `USN` LIKE '$usn' AND `Password` LIKE '$password'";
 $result = mysqli_query($conn, $sql);
 // If User is found than result variable hold complete row of that usn, if not than "null"
-if ( mysqli_num_rows($result) > 0) {
+if (mysqli_num_rows($result) > 0) {
     echo "Student Data Found...!!!<br/>";
     // This below line converts or breaks into associative array from that we can access individual column using column name (Present at top of table)
     $row = mysqli_fetch_assoc($result);
-    echo "Name : ". $row["Name"] ." USN : ". $row["USN"] ." Branch : ". " Password :" . $row["Password"] . " Mobile No :" . $row["Mobile No"] . " Email :" . $row["Email"];
+    echo "Name : " . $row["Name"] . " USN : " . $row["USN"] . " Branch : " . " Password :" . $row["Password"] . " Mobile No :" . $row["Mobile No"] . " Email :" . $row["Email"];
 
-    header("Location: index.html?name=$name&usn=$usn&branch=$branch&password=$password&mobile_no=$mobile_no&email=$email");
-}else {
-    echo "Data not found". mysqli_error($conn);
+    header("Location: CredentialCenter.html?name=$name&usn=$usn&branch=$branch&password=$password&mobile_no=$mobile_no&email=$email");
+} else {
+    echo "Data not found" . mysqli_error($conn);
 }
 
 ?>
--- src/connect/src/signuppage.php
+++ src/connect/src/signuppage.php
@@ -7,10 +7,10 @@ $database = "nexus student data";
 
 $conn = mysqli_connect($servername, $username, $password, $database);
 if (!$conn) {
-    die("Sorry unable to connect!!! ". mysqli_connect_error());
+    die("Sorry unable to connect!!! " . mysqli_connect_error());
 }
 echo "Connection succefull";
-echo"<br/>";
+echo "<br/>";
 // Till here Connection is succefull...
 
 //Fetch data that to be stored in Database
@@ -24,16 +24,16 @@ $sql = "INSERT INTO `signup data` (`Name`, `USN`, `Mobile No` , `Branch`, `Email
 $result = mysqli_query($conn, $sql);
 // Displaying inserted values
 echo "$name , $usn, $mobile, $branch, $email, $password";
-echo"<br/>";
+echo "<br/>";
 
 
 
 $sql = "SELECT * From `signup data`";
 $result = mysqli_query($conn, $sql);
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo "<h2>";
-        echo "Name : ". $row["Name"] ." USN : ". $row["USN"] ." Branch : ". " Password :" . $row["Password"] . " Mobile No :" . $row["Mobile No"] . " Email :" . $row["Email"];
-        echo "</h2><br/>";
-    }
-echo"<br/>";
+while ($row = mysqli_fetch_assoc($result)) {
+    echo "<h2>";
+    echo "Name : " . $row["Name"] . " USN : " . $row["USN"] . " Branch : " . " Password :" . $row["Password"] . " Mobile No :" . $row["Mobile No"] . " Email :" . $row["Email"];
+    echo "</h2><br/>";
+}
+echo "<br/>";
 ?>
